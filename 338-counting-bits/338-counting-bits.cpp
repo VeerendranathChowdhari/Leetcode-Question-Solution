21f00ed8c52bc92f0cb7ad7e15a8aaab135560aa
VeerendranathChowdhari <75413565+VeerendranathChowdhari@users.noreply.github.com>
2022-07-24 07:35:21 +0530

    Time: 4 ms (88.10%), Space: 7.9 MB (72.43%) - LeetHub

--- 338-counting-bits/338-counting-bits.cpp
+++ 338-counting-bits/338-counting-bits.cpp
@@ -5,21 +5,12 @@ public:
         vector<int> dp(n+1);
         
         dp[0]=0; 
+        
         if(n==0)return dp;
-        dp[1]=1;
-        if(n==1)return dp;
-        dp[2]=1;
-        if(n==2)return dp;
-      
-        for(int i=3;i<=n;i++)
+        
+        for(int i=1;i<=n;i++)
         {
-            if(i%2!=0){
-                dp[i]=dp[i-1]+1;
-            }
-            else
-            {
-                dp[i]=dp[i/2];
-            }
+            dp[i]=dp[i/2]+i%2;  // i%2 return 1 for odd, return 0 for even
         }
             
             
