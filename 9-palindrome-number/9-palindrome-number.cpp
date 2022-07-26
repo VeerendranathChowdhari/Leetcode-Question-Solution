4ccbe04a1dd240180abccd22611b0ef91b4ce796
VeerendranathChowdhari <75413565+VeerendranathChowdhari@users.noreply.github.com>
2022-07-26 16:46:58 +0530

    Time: 46 ms (7.41%), Space: 5.8 MB (74.05%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.cpp
+++ 9-palindrome-number/9-palindrome-number.cpp
@@ -2,18 +2,17 @@ class Solution {
 public:
     bool isPalindrome(int x) {
         
-        if(x<0)return false;//(-ve)value is not palindrom
+       if (x<0 || (x!=0 && x%10==0)) return false;//(-ve)value is not palindrom
         
-        long long oldNumb=x;
         long long newNumb=0;
         //reverse the given number
-        while(x>0)
+        while(x>newNumb)
         {
             newNumb= newNumb*10+x%10;
             x=x/10;
         }
-        cout<<oldNumb<<" "<<newNumb;
-        
-        return (oldNumb==newNumb);
+
+        cout<<x<<" "<<newNumb;
+        return (x==newNumb || x==newNumb/10);
     }
 };
