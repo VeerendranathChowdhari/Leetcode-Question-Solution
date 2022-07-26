9bbf0d1451138e4fcc82d2e777308f49669e67c6
VeerendranathChowdhari <75413565+VeerendranathChowdhari@users.noreply.github.com>
2022-07-26 15:48:11 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (56.39%) - LeetHub

--- 67-add-binary/67-add-binary.cpp
+++ 67-add-binary/67-add-binary.cpp
@@ -2,79 +2,34 @@ class Solution {
 public:
     string addBinary(string a, string b) {
         
-        int i=a.length()-1,j=b.length()-1,carry=0;
-        string res="";
-        while(i>=0 && j>=0)
+        long long i=a.length()-1,j=b.length()-1,carry=0;
+        
+        string ans;
+        
+        while(i>=0 || j>=0 || carry)
         {
-            int val=(stoi(to_string(a[i])))-'0'+(stoi(to_string(b[j])))-'0';
-            val+=carry;
-            if(val==3){
-                carry=1;
-                res+="1";
-            }
-            else if(val==2){
-                carry=1;
-                res+="0";
-            }
-            else {
+            if(i>=0)
+            {
+                carry+=(a[i]-'0');
                 
-                res+=to_string(val);
-                 carry=0;
+                i--;
+            }
             
-            }
-            i--,j--;
-        }
-        
-        while(i>=0 )
-        {
-            int val=(stoi(to_string(a[i])))-'0';
-            val+=carry;
-            if(val==3){
-                carry=1;
-                res+="1";
-            }
-            else if(val==2){
-                carry=1;
-                res+="0";
-            }
-            else {
-                cout<<val;
-                 res+=to_string(val);
-                 carry=0;
-            
-            }
-            i--;
-        }
-        
-        while(j>=0 )
-        {
-            int val=(stoi(to_string(b[j])))-'0';
-            val+=carry;
-            if(val==3){
-                carry=1;
-                res+="1";
-            }
-            else if(val==2){
-                carry=1;
-                res+="0";
-            }
-            else {
+            if(j>=0)
+            {
+                carry+=(b[j]-'0');
                 
-                 res+=to_string(val);
-                 carry=0;
-            
+                j--;
             }
-            j--;
+            
+            ans+=(carry%2 + '0');
+            
+            carry=carry/2;
         }
         
-        if(carry>0 )
-        {
-             res+="1";
-        }
+        reverse(ans.begin(),ans.end());
         
-        reverse(res.begin(),res.end());
-        
-        return res;
+        return ans;
         
     }
 };
