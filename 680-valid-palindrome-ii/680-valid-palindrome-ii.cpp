a177477e13e949de61948d4baee58f2e83b11054
VeerendranathChowdhari <75413565+VeerendranathChowdhari@users.noreply.github.com>
2022-07-26 06:48:16 +0530

    Time: 59 ms (82.99%), Space: 19.6 MB (75.84%) - LeetHub

--- 680-valid-palindrome-ii/680-valid-palindrome-ii.cpp
+++ 680-valid-palindrome-ii/680-valid-palindrome-ii.cpp
@@ -1,60 +1,13 @@
 class Solution {
 public:
     bool validPalindrome(string s) {
-     
-        if(s.length()==0 || s.length()==1 )return true;
-        
-        int low=0,high=s.length()-1;
-        char first='0',second='0';
-        
-        while(low<high)
-        {
-            if(s[low]!=s[high]){
-                
-                first=s[low];
-                second=s[high];
-                break;
+        for (int i = 0, j = s.size() - 1; i < j; i++, j--)
+            if (s[i] != s[j]) {
+                int i1 = i, j1 = j - 1, i2 = i + 1, j2 = j;
+                while (i1 < j1 && s[i1] == s[j1]) {i1++; j1--;};
+                while (i2 < j2 && s[i2] == s[j2]) {i2++; j2--;};
+                return i1 >= j1 || i2 >= j2;
             }
-            low++,high--;
-        }
-        
-        cout<<s[low]<<" "<<s[high]<<low<<endl;
-        
-        if(first=='0')return true;
-        
-        for(int i=0;i<2;i++)
-        {
-            char val;
-            if(i==0)val=first;
-            else val=second;
-            
-            int low=0,high=s.length()-1,l=0,h=0;
-            while(low<=high)
-            {
-                if(s[low]==s[high])
-                {
-                    low++,high--;
-                }
-                
-                else if(s[low]==val && l==0){
-                    low++;l++;h++;
-                }
-                else if(s[high]==val && h==0)
-                {
-                    high--; l++;  h++;
-                }
-                
-                else{
-                    
-                    if(s[low]!=s[high])break;
-                    low++,high--;
-                }   
-            }
-        cout<<s[low]<<" "<<s[high]<<val;
-        if(low>=high)return true;
-            
-        }
-        
-        return false;
+        return true;
     }
 };
