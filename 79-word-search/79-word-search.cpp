1595f75f117cb3349cf574cbce855a83934969ee
VeerendranathChowdhari <75413565+VeerendranathChowdhari@users.noreply.github.com>
2022-07-29 14:52:34 +0530

    Time: 561 ms (51.89%), Space: 8 MB (35.71%) - LeetHub

--- 79-word-search/79-word-search.cpp
+++ 79-word-search/79-word-search.cpp
@@ -1,25 +1,27 @@
 class Solution {
 public:
     int row,col;
-    bool helper(vector<vector<char>>& board, string word,int n,int i ,int j,vector<vector<bool>>&vis)
+    bool helper(vector<vector<char>>& board, string word,int n,int i ,int j)
     {
         
         if(n==0 && board[i][j]==word[n])return true;//last character matches
          
         if(board[i][j]!=word[n])return false;//current word not matches
         
-        vis[i][j]=true;
+        char ch=board[i][j];
+        board[i][j]='9';
         
-        if((i-1>=0 && !vis[i-1][j] && helper(board,word,n-1,i-1,j,vis)))return true;
+        //search in top
+        if((i-1>=0 && '9'!=board[i-1][j] && helper(board,word,n-1,i-1,j)))return true;
+        //search in left
+        else if (j-1>=0 && '9'!=board[i][j-1] && helper(board,word,n-1,i,j-1))return true;
+        //search in bottom
+        else if(i+1<row && '9'!=board[i+1][j]  && helper(board,word,n-1,i+1,j))return true;
+        //search in right
+        else if(j+1<col && '9'!=board[i][j+1] && helper(board,word,n-1,i,j+1))return true;
         
-        else if (j-1>=0 && !vis[i][j-1] && helper(board,word,n-1,i,j-1,vis))return true;
+        board[i][j]=ch;
         
-        else if(i+1<row && !vis[i+1][j]  && helper(board,word,n-1,i+1,j,vis))return true;
-        
-        else if(j+1<col && !vis[i][j+1] && helper(board,word,n-1,i,j+1,vis))return true;
-        
-        vis[i][j]=false;
-           
         return false;
     }
     
@@ -32,15 +34,13 @@ public:
         
         col=board[0].size();
         
-        vector<vector<bool>>vis(row,vector<bool>(col,false));
-        
         for(int i=0;i<board.size();i++)
         {
             for(int j=0;j<board[i].size();j++)
             {
                 if(board[i][j]== word.back())
                 {
-                    if(helper(board,word,n-1,i ,j,vis))return true;
+                    if(helper(board,word,n-1,i ,j))return true;
                 }
             }
         }
