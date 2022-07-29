60752fd8271a136023237cdecbe5201762653af0
VeerendranathChowdhari <75413565+VeerendranathChowdhari@users.noreply.github.com>
2022-07-29 15:02:14 +0530

    Time: 952 ms (22.64%), Space: 8.1 MB (35.71%) - LeetHub

--- 79-word-search/79-word-search.cpp
+++ 79-word-search/79-word-search.cpp
@@ -4,7 +4,7 @@ public:
     bool helper(vector<vector<char>>& board, string word,int n,int i ,int j)
     {
         
-        if(n==0 && board[i][j]==word[n])return true;//last character matches
+        if(n==word.length()-1 && board[i][j]==word[n])return true;//last character matches
          
         if(board[i][j]!=word[n])return false;//current word not matches
         
@@ -14,13 +14,13 @@ public:
         //board[i][j]='9';
         
         //search in top
-        if((i-1>=0 && helper(board,word,n-1,i-1,j)))return true;
+        if((i-1>=0 && helper(board,word,n+1,i-1,j)))return true;
         //search in left
-        else if (j-1>=0 &&helper(board,word,n-1,i,j-1))return true;
+        else if (j-1>=0 &&helper(board,word,n+1,i,j-1))return true;
         //search in bottom
-        else if(i+1<row && helper(board,word,n-1,i+1,j))return true;
+        else if(i+1<row && helper(board,word,n+1,i+1,j))return true;
         //search in right
-        else if(j+1<col &&  helper(board,word,n-1,i,j+1))return true;
+        else if(j+1<col &&  helper(board,word,n+1,i,j+1))return true;
         
         //board[i][j]=ch;
         
@@ -42,9 +42,9 @@ public:
         {
             for(int j=0;j<board[i].size();j++)
             {
-                if(board[i][j]== word.back())
+                if(board[i][j]== word[0])
                 {
-                    if(helper(board,word,n-1,i ,j))return true;
+                    if(helper(board,word,0,i ,j))return true;
                 }
             }
         }
